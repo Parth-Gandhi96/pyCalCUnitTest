75aed3616be5da5abba8373861dfb00f02af40bf
parth <gandhi.bapu96@gmail.com>
2021-03-20 10:14:37 -0400

    Task 2: Multiplication Unit Test code added

--- testingCode.py
+++ testingCode.py
@@ -29,7 +29,8 @@ class TestAdd(unittest.TestCase):
                 self.assertEqual(calculator.add(val1[i], val2[i]), res[i])
                 passed +=1
             except Exception:
-                failed+=1;
+                print("Addition : "+str(calculator.multiply(val1[i], val2[i]))+"!="+str(res[i]))
+                failed+=1
         print("Addition test-report : Passed:"+str(passed)+"   Failed:"+str(failed))
 
     def test_subtract(self):
@@ -56,7 +57,8 @@ class TestAdd(unittest.TestCase):
                 self.assertEqual(calculator.subtract(val1[i], val2[i]), res[i])
                 passed += 1
             except Exception:
-                failed += 1;
+                print("Subtraction : "+str(calculator.multiply(val1[i], val2[i]))+"!="+str(res[i]))
+                failed += 1
         print("Subtraction test-report : Passed:" + str(passed) + "   Failed:" + str(failed))
 
     def test_divide(self):
@@ -84,9 +86,38 @@ class TestAdd(unittest.TestCase):
                 self.assertEqual(("{:."+str(l)+"f}").format(round(calculator.divide(val2[i], val1[i]),l)), str(res[i]))
                 passed += 1
             except Exception:
-                print(str(("{:."+str(l)+"f}").format(round(calculator.divide(val2[i], val1[i]),l))) +" != "+str(res[i]))
+                print("Division : "+str(("{:."+str(l)+"f}").format(round(calculator.divide(val2[i], val1[i]),l))) +" != "+str(res[i]))
                 failed += 1;
         print("Division test-report : Passed:" + str(passed) + "   Failed:" + str(failed))
+
+    def test_multiply(self):
+        '''Testing multiplication menthod'''
+        filePath = 'testCases/Unit Test Multiplication.csv'
+        val1 = []
+        val2 = []
+        res = []
+        with open(filePath) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in csv_reader:
+                if line_count != 0:
+                    val1.append(float(row[0]))
+                    val2.append(float(row[1]))
+                    res.append(float(row[2]))
+                line_count += 1
+
+        calculator = cal("Multiplication Calculator Testing")
+        passed = 0
+        failed = 0
+        for i in range(0, len(val1)):
+            try:
+                self.assertEqual(calculator.multiply(val1[i], val2[i]), res[i])
+                passed += 1
+            except Exception:
+                print("Multiplication : "+str(calculator.multiply(val1[i], val2[i]))+"!="+str(res[i]))
+                failed += 1
+        print("Multiplication test-report : Passed:" + str(passed) + "   Failed:" + str(failed))
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
